718a0dae0484ef0517403941bc74710aa26f440e
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-06-16 13:18:01 +0200

    drawing approach 1 - poc

--- gui/src/main/java/pl/marcinchwedczuk/paintme/gui/mainwindow/MainWindow.java
+++ gui/src/main/java/pl/marcinchwedczuk/paintme/gui/mainwindow/MainWindow.java
@@ -1,28 +1,22 @@
 package pl.marcinchwedczuk.paintme.gui.mainwindow;
 
-import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.TransferMode;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import pl.marcinchwedczuk.paintme.domain.Util;
-import pl.marcinchwedczuk.paintme.gui.dragdrop.HelloDragAndDrop;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainWindow implements Initializable {
     public static MainWindow showOn(Stage window) {
@@ -47,8 +41,50 @@ public class MainWindow implements Initializable {
         }
     }
 
+    @FXML
+    private ImageView drawingCanvas;
+    @FXML
+    private Pane drawingCanvasContainer;
+
+    private WritableImage image;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("MainWindow Initailized");
+        System.out.println("Initialized");
+
+        drawingCanvasContainer.widthProperty().addListener((o, oldValue, newValue) -> {
+            resizeDrawingCanvas();
+        });
+        drawingCanvasContainer.heightProperty().addListener((o, oldValue, newValue) -> {
+            resizeDrawingCanvas();
+        });
+
+        drawingCanvasContainer.setOnMouseClicked(e -> {
+            drawRandom(e.getX(), e.getY());
+        });
+
+        drawingCanvasContainer.setOnMouseMoved(e -> {
+            //if (e.getButton() == MouseButton.PRIMARY) {
+                drawRandom(e.getX(), e.getY());
+            //}
+        });
+
+        resizeDrawingCanvas();
+    }
+
+    @FXML
+    void buttonTest(ActionEvent event) {
+    }
+
+    private void resizeDrawingCanvas() {
+        double w = Math.max(1, drawingCanvasContainer.getWidth());
+        double h = Math.max(1, drawingCanvasContainer.getHeight());
+
+        image = new WritableImage((int)w, (int)h);
+        drawingCanvas.setImage(image);
+    }
+
+    private void drawRandom(double x, double y) {
+        image.getPixelWriter().setColor((int)x, (int)y, Color.BLACK);
     }
 }
