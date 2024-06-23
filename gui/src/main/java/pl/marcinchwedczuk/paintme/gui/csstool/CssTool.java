901244343a6f7aec32faca1ea2b513b57e61b708
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-06-23 19:11:19 +0200

    loading list of css props in css tool

--- gui/src/main/java/pl/marcinchwedczuk/paintme/gui/csstool/CssTool.java
+++ gui/src/main/java/pl/marcinchwedczuk/paintme/gui/csstool/CssTool.java
@@ -2,6 +2,8 @@ package pl.marcinchwedczuk.paintme.gui.csstool;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -16,6 +18,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.*;
 
@@ -79,11 +82,41 @@ public class CssTool implements Initializable {
                 BorderPane.setAlignment(control, Pos.CENTER);
 
                 if (control instanceof Labeled) {
-                    ((Labeled)control).setText("Foobar");
+                    ((Labeled) control).setText("Foobar");
                 }
 
+                // Get list of properties
+                StringBuilder sb = new StringBuilder();
+
+                List<? extends Styleable> ll = List.of(control);
+
+                Class<?> current = controlClass;
+                while (!current.getSimpleName().equals("Object")) {
+                    Method m = current.getMethod("getClassCssMetaData");
+                    if (m != null) {
+                        List<CssMetaData<Control, ?>> stylables = (List<CssMetaData<Control, ?>>) m.invoke(null);
+                        for (CssMetaData<Control, ?> entry : stylables) {
+                            sb.append(entry.getProperty()).append('\n')
+                                    .append('\t').append("init: ").append(entry.getInitialValue(control)).append('\n');
+
+                            var subs = (List<CssMetaData<Control, ?>>) (Object) entry.getSubProperties();
+                            if (subs != null) {
+                                for (var sub : subs) {
+                                    sb.append('\t').append(sub.getProperty()).append(" init: ").append(sub.getInitialValue(control)).append('\n');
+                                }
+                            }
+                        }
+
+                        break;
+                    }
+
+                    current = current.getSuperclass();
+                }
+
+                helpText.setText(sb.toString());
+
             } catch (Exception e) {
-                StringWriter out    = new StringWriter();
+                StringWriter out = new StringWriter();
                 PrintWriter writer = new PrintWriter(out);
 
                 e.printStackTrace(writer);
@@ -97,6 +130,8 @@ public class CssTool implements Initializable {
             if (event.getCode() == KeyCode.ENTER && event.isControlDown()) {
                 System.out.println("RELOADED CSS");
                 InMemoryCss.cssText = cssText.getText();
+
+                if (controlContainer.getChildren().isEmpty()) return;
 
                 ObservableList<String> stylesheets = ((Control) controlContainer.getChildren().get(0)).getStylesheets();
                 stylesheets.clear();
